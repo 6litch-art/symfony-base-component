1bb768b09e36efee220d0dafccbf7b5fe230518a
Marco Meyer (www) <marco.meyerconde@gmail.com>
2021-09-29 15:04:15 +0200

    Update css and translatable fields

--- src/Field/Type/TranslatableType.php
+++ src/Field/Type/TranslatableType.php
@@ -2,12 +2,42 @@
 
 namespace Base\Field\Type;
 
+use Base\Database\TranslatableInterface;
 use Base\Field\Traits\SelectTypeInterface;
 use Base\Field\Traits\SelectTypeTrait;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class TranslatableType extends AbstractType
 {
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'compound' => false
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'translatable';
+    }
+
 }
