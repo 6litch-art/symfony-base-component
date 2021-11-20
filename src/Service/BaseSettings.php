0bcea8b68281599389016209eb31a44afd96caeb
Marco Meyer (www) <marco.meyerconde@gmail.com>
2021-11-20 23:30:13 +0100

    Update base Settings

--- src/Service/BaseSettings.php
+++ src/Service/BaseSettings.php
@@ -24,31 +24,11 @@ class BaseSettings
         $this->localeProvider = $localeProvider;
     }
 
-    public function protocol   () { return filter_var($this->get("base.settings.use_https"  )) ? "https" : "http"; }
-    public function maintenance() { return $this->get("base.settings.maintenance"); }
-
-    public function logo     (?string $locale = null) { return $this->get("base.settings.logo",      $locale); }
-    public function title    (?string $locale = null) { return $this->get("base.settings.title",     $locale); }
-    public function slogan   (?string $locale = null) { return $this->get("base.settings.slogan",    $locale); }
-    public function mail     (?string $locale = null) { return $this->get("base.settings.mail",      $locale); }
-    public function mailName (?string $locale = null) { return $this->get("base.settings.mail_name", $locale); }
-    public function birthdate(?string $locale = null) : \DateTime { return new \DateTime($this->get("base.settings.birthdate", $locale)); }
-
-    public function age(?string $locale = null) { return $this->getAge($locale); }
-    public function getAge     (?string $locale = null)
-    { 
-        $birthdate = $this->birthdate($locale)->format("Y");
-        return (date("Y") <= $birthdate) ? date("Y") : date("$birthdate-Y");
-    }
-
-    public function url($url, $packages = null) { return $this->getUrl($url, $packages); }
-    public function getUrl($url, $packages = null)
-    {
-        return $this->packages->getUrl($url, $packages);
-    }
-
-    public function domain     (int $level = 0, ?string $locale = null) { return $this->getDomain($level, $locale); }
-    public function getDomain(int $level = 0, ?string $locale = null)
+    public function mail       (?string $locale = null) : string { return $this->get("base.settings.mail",      $locale); }
+    public function mail_name  (?string $locale = null) : string { return $this->get("base.settings.mail.name", $locale); }
+    public function protocol   (?string $locale = null) : string { return filter_var($this->get("base.settings.domain.https", $locale)) ? "https" : "http"; }
+    public function maintenance(?string $locale = null) : bool   { return filter_var($this->get("base.settings.maintenance", $locale)); }
+    public function domain     (int $level = 0, ?string $locale = null) : string
     {
         $domain = $this->get("base.settings.domain",       $locale);
         while($level-- > 0)
@@ -56,4 +36,15 @@ class BaseSettings
 
         return $domain;
     }
+
+    public function logo     (?string $locale = null) : string    { return $this->get("base.settings.logo",      $locale); }
+    public function title    (?string $locale = null) : string    { return $this->get("base.settings.title",     $locale); }
+    public function slogan   (?string $locale = null) : string    { return $this->get("base.settings.slogan",    $locale); }
+    public function birthdate(?string $locale = null) : \DateTime { return new \DateTime($this->get("base.settings.birthdate", $locale)); }
+    public function age(?string $locale = null) : string {
+
+        $birthdate = $this->birthdate($locale)->format("Y");
+        return (date("Y") <= $birthdate) ? date("Y") : date("$birthdate-Y");
+    }
+
 }
