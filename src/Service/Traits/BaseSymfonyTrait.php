3bec290097d9fad8287fa039d3b0e943d98b499b
Marco Meyer (www) <marco.meyerconde@gmail.com>
2021-12-01 11:17:05 +0100

    Fix php8 compatibility

--- src/Service/Traits/BaseSymfonyTrait.php
+++ src/Service/Traits/BaseSymfonyTrait.php
@@ -224,8 +224,9 @@ trait BaseSymfonyTrait
         $controller = explode("::", $array[0])[0];
 
         $parents = [];
+
         $parent = $controller;
-        while(( $parent = get_parent_class($parent) ))
+        while(class_exists($parent) && ( $parent = get_parent_class($parent)))
             $parents[] = $parent;
 
         $eaParents = array_filter($parents, fn($c) => str_starts_with($c, "EasyCorp\Bundle\EasyAdminBundle"));
