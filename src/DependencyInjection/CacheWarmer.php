2cf3d564cbcf94ff8c13f7dc6af05573b8dfa2af
Marco Meyer (www) <marco.meyerconde@gmail.com>
2022-03-20 17:04:16 +0100

    Add annotation to cache warmer

--- src/DependencyInjection/CacheWarmer.php
+++ src/DependencyInjection/CacheWarmer.php
@@ -3,14 +3,16 @@
 namespace Base\DependencyInjection;
 
 use Base\Service\IconService;
+use Base\Annotations\AnnotationReader;
 use Symfony\Component\HttpKernel\CacheWarmer\CacheWarmerInterface;
 
 class CacheWarmer implements CacheWarmerInterface
 {
-    public function __construct(IconService $iconService)
+    public function __construct(IconService $iconService, AnnotationReader $annotationReader)
     {
         $this->shellVerbosity = getenv("SHELL_VERBOSITY");
         $this->iconService = $iconService;
+        $this->annotationReader = $annotationReader;
     }
 
     public function isOptional():bool { return false; }
@@ -18,6 +20,6 @@ class CacheWarmer implements CacheWarmerInterface
     {
         if($this->shellVerbosity > 0) echo " // Warming up cache... Base bundle".PHP_EOL.PHP_EOL;
 
-        return [get_class($this->iconService)];
+        return [get_class($this->iconService), get_class($this->annotationReader)];
     }
 }
