e508cbbf063295c7a538d4b58029eeeda6e00bf5
Server LTO <www-data@latoucheoriginale.fr>
2022-06-19 18:58:21 +0000

    Update

---
+++ src/Config/Action.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Base\Config;
+
+class Action extends \EasyCorp\Bundle\EasyAdminBundle\Config\Action
+{
+    public const GOTO_PREV = 'gotoPrev';
+    public const GOTO_NEXT = 'gotoNext';
+}
