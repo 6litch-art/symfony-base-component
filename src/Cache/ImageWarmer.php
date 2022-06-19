e2b9b426adf30959d2b1632d10013fe5a2b33b9b
Server LTO <www-data@latoucheoriginale.fr>
2022-06-19 16:02:26 +0000

    Update

--- src/Cache/ImageWarmer.php
+++ src/Cache/ImageWarmer.php
@@ -11,7 +11,8 @@ class ImageWarmer implements CacheWarmerInterface
     public function isOptional():bool { return true; }
     public function warmUp($cacheDir): array
     {
-        if($this->shellVerbosity > 0 && php_sapi_name() == "cli") echo " // Warming up cache... Image & crops".PHP_EOL.PHP_EOL;
+        if($this->shellVerbosity > 0 && php_sapi_name() == "cli")
+            echo " // Warming up cache... Image & crops".PHP_EOL.PHP_EOL;
 
         return [];
     }
