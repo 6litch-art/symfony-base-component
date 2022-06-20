6153b33cf491b9aa3c39e16fe145418ca2ffb73e
Server LTO <www-data@latoucheoriginale.fr>
2022-06-20 14:36:08 +0000

    Update

--- src/Config/Actions.php
+++ src/Config/Actions.php
@@ -35,26 +35,26 @@ class Actions extends \EasyCorp\Bundle\EasyAdminBundle\Config\Actions
         return parent::createBuiltInAction($pageName, $actionName);
     }
 
-    protected function doAddAction(string $pageName, Action|string $actionNameOrObject, bool $isBatchAction = false)
-    {
-        $actionName = \is_string($actionNameOrObject) ? $actionNameOrObject : (string) $actionNameOrObject;
-        $action = \is_string($actionNameOrObject) ? $this->createBuiltInAction($pageName, $actionNameOrObject) : $actionNameOrObject;
+    // protected function doAddAction(string $pageName, Action|string $actionNameOrObject, bool $isBatchAction = false)
+    // {
+    //     $actionName = \is_string($actionNameOrObject) ? $actionNameOrObject : (string) $actionNameOrObject;
+    //     $action = \is_string($actionNameOrObject) ? $this->createBuiltInAction($pageName, $actionNameOrObject) : $actionNameOrObject;
 
-        if (null !== $this->dto->getAction($pageName, $actionName)) {
-            throw new \InvalidArgumentException(sprintf('The "%s" action already exists in the "%s" page, so you can\'t add it again. Instead, you can use the "updateAction()" method to update any options of an existing action.', $actionName, $pageName));
-        }
+    //     if (null !== $this->dto->getAction($pageName, $actionName)) {
+    //         throw new \InvalidArgumentException(sprintf('The "%s" action already exists in the "%s" page, so you can\'t add it again. Instead, you can use the "updateAction()" method to update any options of an existing action.', $actionName, $pageName));
+    //     }
 
-        $actionDto = $action->getAsDto();
-        if ($isBatchAction) {
-            $actionDto->setType(Action::TYPE_BATCH);
-        }
+    //     $actionDto = $action->getAsDto();
+    //     if ($isBatchAction) {
+    //         $actionDto->setType(Action::TYPE_BATCH);
+    //     }
 
-        if (Crud::PAGE_INDEX === $pageName && Action::DELETE === $actionName) {
-            $this->dto->prependAction($pageName, $actionDto);
-        } else {
-            $this->dto->appendAction($pageName, $actionDto);
-        }
+    //     if (Crud::PAGE_INDEX === $pageName && Action::DELETE === $actionName) {
+    //         $this->dto->prependAction($pageName, $actionDto);
+    //     } else {
+    //         $this->dto->appendAction($pageName, $actionDto);
+    //     }
 
-        return $this;
-    }
+    //     return $this;
+    // }
 }
