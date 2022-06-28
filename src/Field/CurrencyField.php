9ec368ae6d58fb557fb5ba6288bd44961891ef71
Server LTO <www-data@latoucheoriginale.fr>
2022-06-28 11:08:12 +0000

    Update actions

--- src/Field/CurrencyField.php
+++ src/Field/CurrencyField.php
@@ -4,12 +4,9 @@ namespace Base\Field;
 
 use Base\Field\Type\CurrencyType;
 use EasyCorp\Bundle\EasyAdminBundle\Contracts\Field\FieldInterface;
-use EasyCorp\Bundle\EasyAdminBundle\Field\FieldTrait;
 
 class CurrencyField extends SelectField implements FieldInterface
 {
-    use FieldTrait;
-
     public static function new(string $propertyName, ?string $label = null): self
     {
         return (new self())
