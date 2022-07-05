fce27f8f6ecc7256c035a9b74a780a6aff25f976
Marco Meyer (www) <marco.meyerconde@gmail.com>
2022-07-05 09:10:55 +0200

    Update translator

--- src/Service/TranslatorInterface.php
+++ src/Service/TranslatorInterface.php
@@ -12,6 +12,8 @@ interface TranslatorInterface extends \Symfony\Contracts\Translation\TranslatorI
     public function transQuiet (TranslatableMessage|string $id, array $parameters = [], ?string $domain = null, ?string $locale = null, bool $recursive = true):?string;
     public function transExists(TranslatableMessage|string $id,                         ?string $domain = null, ?string $locale = null):bool;
 
+    public function time(int $time): string;
+
     public function enum(?string $value, string $class, string|array $options = Translator::TRANSLATION_SINGULAR): ?string;
     public function enumExists(?string $value, string $class, string|array $options = Translator::TRANSLATION_SINGULAR): bool;
 
