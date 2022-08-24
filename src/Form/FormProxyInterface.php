fd162f297d1f620ee853b8ec6ea889320f86b75b
Marco Meyer (www) <marco.meyerconde@gmail.com>
2022-08-24 04:45:32 +0200

    Update form classes

--- src/Form/FormProxyInterface.php
+++ src/Form/FormProxyInterface.php
@@ -2,7 +2,9 @@
 
 namespace Base\Form;
 
+use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 interface FormProxyInterface
 {
@@ -10,9 +12,11 @@ interface FormProxyInterface
     public function add(string $name, ?FormInterface $form): static;
     public function remove(string $name): static;
     public function has(string $name):bool;
-
     public function get(string $name);
     public function getData(string $name): mixed;
 
+    public function create(string $name, string $type = FormType::class, mixed $data = null, array $options = []): ?FormInterface;
     public function submit(string $name, string|array|null $submittedData, bool $clearMissing = true): ?FormInterface;
+    public function process(string $name, Request $request): ?FormInterface;
+    public function hydrate(string $name, object $entity): mixed;
 }
