108441059a92cd8770fbc0131e9cdb18e9087878
Marco Meyer (www) <marco.meyerconde@gmail.com>
2022-10-27 12:44:02 +0200

    Fix maternity issue

--- src/Service/MaternityServiceInterface.php
+++ src/Service/MaternityServiceInterface.php
@@ -4,12 +4,11 @@ namespace Base\Service;
 
 use DateTime;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
-use Symfony\Component\HttpKernel\Event\ResponseEvent;
 
 interface MaternityServiceInterface
 {
     public function getBirthdate(?string $locale = null) : ?DateTime;
-    public function isBorn(?string $locale = null) : bool;
+    public function isBorn(?string $locale = null) : ?bool;
     public function getAge(?string $locale = null) : string;
 
     public function redirectOnDeny(?RequestEvent $event = null): bool;
