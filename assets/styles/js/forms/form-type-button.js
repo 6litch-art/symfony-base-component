6b5ad63f526ffd319f6a974820b945592490fb5a
Marco Meyer (www) <marco.meyerconde@gmail.com>
2023-01-25 15:37:54 +0100

    Simplfiy button js confirm box

--- assets/styles/js/forms/form-type-button.js
+++ assets/styles/js/forms/form-type-button.js
@@ -3,38 +3,16 @@ window.addEventListener("load.form_type", function () {
     document.querySelectorAll("[data-button-field]").forEach((function (el) {
 
         var id = el.getAttribute("data-button-field");
-        var confirmation = false;
-
+        
         var onConfirmation        = el.getAttribute("data-button-confirmation") ?? false;
         if(onConfirmation) {
 
             $("#"+id).on("click", function (e) {
 
-                if(confirmation) return true;  
-                else { 
-    
-                    $('#'+id+'-modal').modal('show');
-                    e.preventDefault();
-                }
-    
-                confirmation = false;
+                $('#'+id+'-modal').modal('show');
+                e.preventDefault();
+
                 return false;
-            });
-
-            $("#"+id+"-confirm").on("click", function (e) {
-
-                $('#'+id+'-modal').modal('hide');
-                confirmation = true;
-
-                $("#"+id).trigger("click");
-
-            });
-
-            $("#"+id+"-cancel").on("click", function (e) {
-
-                $('#'+id+'-modal').modal('hide');
-                confirmation = false;
-
             });
         }
     }));
