370e536ec6c713946f5482e23b6c98ace3f270fb
Marco Meyer <marco.meyer@glitchr.io>
2023-02-08 07:18:29 +0900

    Implement hot parameter bag for CLI commands

--- src/Subscriber/HotParameterBagSubscriber.php
+++ src/Subscriber/HotParameterBagSubscriber.php
@@ -4,6 +4,8 @@ namespace Base\Subscriber;
 
 use Base\Service\HotParameterBag;
 use Base\Service\SettingBagInterface;
+use Symfony\Component\Console\ConsoleEvents;
+use Symfony\Component\Console\Event\ConsoleCommandEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -27,13 +29,34 @@ class HotParameterBagSubscriber implements EventSubscriberInterface
 
     public static function getSubscribedEvents(): array
     {
-        return [KernelEvents::REQUEST  => ['onKernelRequest', 256]];
+        return [
+            ConsoleEvents::COMMAND => ['onCommandRequest', 2048],
+            KernelEvents::REQUEST  => ['onKernelRequest', 256]
+        ];
+    }
+
+    public function onCommandRequest(ConsoleCommandEvent $event)
+    {
+        if(!$this->parameterBag instanceof HotParameterBag) return;
+        if($this->parameterBag->isReady()) return;
+
+        array_map_recursive(function($setting) {
+
+            if($setting === null) return;
+            if($setting->getBag() === null) return;
+
+            $this->parameterBag->add([$setting->getBag() => $setting->getValue()]);
+
+        }, $this->settingBag->allRaw(true, true));
+
+        $this->parameterBag->markAsReady();
     }
 
     public function onKernelRequest(RequestEvent $event)
     {
         if(!$event->isMainRequest()) return;
         if(!$this->parameterBag instanceof HotParameterBag) return;
+        if($this->parameterBag->isReady()) return;
 
         array_map_recursive(function($setting) {
 
