ada467f2ab011c0c1337601006d1cffdadc5b179
Server LTO <www-data@latoucheoriginale.fr>
2023-04-04 07:17:57 +0000

    Update obfuscator

--- src/Service/Model/CacheableInterface.php
+++ src/Service/Model/CacheableInterface.php
@@ -6,7 +6,7 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 interface CacheableInterface
 {
-    public function __toKey(string ...$context): string;
+    public function __toKey(?string ...$context): string;
     public function __toKeyTTL() : ?int;
     public function __toKeyTags() : array;
 }
