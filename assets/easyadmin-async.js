7855ca4f169c3bbc39a40a2356c0950df1c6a0c6
Marco Meyer <marco.meyer@glitchr.io>
2023-05-01 19:14:36 +0900

    Update assets

--- assets/easyadmin-async.js
+++ assets/easyadmin-async.js
@@ -1,7 +1,8 @@
 import './styles/easyadmin-async.scss';
 
-var spinnerTimeout = setTimeout(function() { $(".content").addClass("spinner"); }, 1000);
-$(window).on("load", function(e) {
+var spinnerTimeout = setTimeout(function () {
+    $(".content").addClass("spinner"); }, 1000);
+$(window).on("load", function (e) {
 
     $(".content").addClass("spinner");
     $(".spinner").addClass("spinner loaded");
@@ -10,13 +11,15 @@ $(window).on("load", function(e) {
 
 //
 // Apply bootstrap form validation
-window.addEventListener('load', function(event) {
+window.addEventListener('load', function (event) {
 
-    $("form :input").on("change", function () { // Reactivate button when a form is changed
+    $("form :input").on("change", function () {
+ // Reactivate button when a form is changed
         $(".page-actions button").removeAttr("disabled").removeClass("disabled");
     });
 
-    $("form :input").on("input", function () { // Reactivate button when a form is changed
+    $("form :input").on("input", function () {
+ // Reactivate button when a form is changed
         $(".page-actions button").removeAttr("disabled").removeClass("disabled");
     });
 
@@ -25,7 +28,7 @@ window.addEventListener('load', function(event) {
         $(".page-actions button").removeAttr("disabled").removeClass("disabled");
     });
 
-    $("form select").each(function() {
+    $("form select").each(function () {
         observer.observe(this, {subtree: true, childList: true});
     });
 
