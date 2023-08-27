700a2d3fb87dbea95808aae24b79873612f3a8c8
Marco Meyer <marco.meyer@glitchr.io>
2023-08-27 23:26:26 +0900

    Merge branch user-update into 2.0

--- src/Functions.php
+++ src/Functions.php
@@ -4098,24 +4098,6 @@ namespace {
             (is_string($datetime) ? (new DateTime())->modify($datetime) : (clone $datetime));
     }
 
-    function rrmdir($dir) { 
-        
-        if (is_dir($dir)) { 
-            $objects = scandir($dir);
-            foreach ($objects as $object) { 
-
-                if ($object != "." && $object != "..") { 
-
-                    if (is_dir($dir. DIRECTORY_SEPARATOR .$object) && !is_link($dir."/".$object))
-                        rrmdir($dir. DIRECTORY_SEPARATOR .$object);
-                else
-                    unlink($dir. DIRECTORY_SEPARATOR .$object); 
-                } 
-            }
-            rmdir($dir); 
-        } 
-    }
-
     function daydiff(null|string|int|DateTime $datetime): ?int
     {
         if ($datetime == null) {
