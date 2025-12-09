af5a92f39ceb7631c95b5343a9ed541f27f74946
Marco Meyer-Conde <marco.meyerconde@gmail.com>
2025-12-10 00:41:56 +0900

    update EA actions + fix request deprecation

--- src/Inspector/BundleDataCollector.php
+++ src/Inspector/BundleDataCollector.php
@@ -251,9 +251,9 @@ class BundleDataCollector extends AbstractDataCollector
         if (class_exists(EasyAdminBundle::class)) {
             $data[$this->getBundleFormattedName(EasyAdminBundle::class)] = $context ? [
                 'CRUD Controller FQCN' => $context->getCrud()?->getControllerFqcn(),
-                'CRUD Action' => $context->getRequest()->get(EA::CRUD_ACTION),
-                'Entity ID' => $context->getRequest()->get(EA::ENTITY_ID),
-                'Sort' => $context->getRequest()->get(EA::SORT),
+                'CRUD Action' => $context->getRequest()->query->get(EA::CRUD_ACTION),
+                'Entity ID' => $context->getRequest()->query->get(EA::ENTITY_ID),
+                'Sort' => $context->getRequest()->query->get(EA::SORT),
             ] : [];
         }
 
