ec78102dacb52f1b97c4e0f90727111dfe741988
Marco Meyer <marco.meyer@glitchr.io>
2026-03-05 23:14:13 +0100

    fix timemachine

--- src/Service/TimeMachine.php
+++ src/Service/TimeMachine.php
@@ -395,7 +395,7 @@ class TimeMachine extends BackupManager implements TimeMachineInterface
      * @param int|array $storageNames
      * @param string|null $prefix
      * @param int $cycle
-     * @return true
+     * @return bool
      * @throws CompressorTypeNotSupported
      * @throws ConfigFieldNotFound
      * @throws ConfigNotFoundForConnection
@@ -403,7 +403,7 @@ class TimeMachine extends BackupManager implements TimeMachineInterface
      * @throws FilesystemException
      * @throws FilesystemTypeNotSupported
      */
-    public function backup(null|string|array $databases, int|array $storageNames = [], bool $userInfo = false, ?string $prefix = null, int $cycle = -1)
+    public function backup(null|string|array $databases, int|array $storageNames = [], bool $userInfo = false, ?string $prefix = null, int $cycle = -1): bool
     {
         $prefix = $prefix ?? $this->environment;
         $this->output?->section()->writeln("<info>Backup procedure started for </info> \"" . $prefix. "\"");
@@ -535,9 +535,9 @@ class TimeMachine extends BackupManager implements TimeMachineInterface
             rrmdir($outputDir);
         }
 
-	if( empty($output) ) {
-	    return false;
-	}
+        if( empty($output) ) {
+            return false;
+        }
 
         foreach ($destinations as $id => $destination) {
 
