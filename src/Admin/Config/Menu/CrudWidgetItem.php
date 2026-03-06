75b9990fe7796d5ddc7db16cb7a4fe47a555c099
Marco Meyer <marco.meyer@glitchr.io>
2026-03-06 23:33:32 +0100

    update widget items

--- src/Admin/Config/Menu/CrudWidgetItem.php
+++ src/Admin/Config/Menu/CrudWidgetItem.php
@@ -123,7 +123,7 @@ final class CrudWidgetItem implements MenuItemInterface
                 WidgetItem::$adminUrlGenerator->setController($crudControllerFqcn);
                 // 2. ...otherwise, find the CRUD controller from the entityFqcn
             } else {
-                $crudControllers = WidgetItem::$adminContextProvider->getContext()->getCrudControllers();
+                $crudControllers = WidgetItem::$adminContextProvider->getContext()->getCrud();
                 if (null === $controllerFqcn = AbstractCrudController::getCrudControllerFqcn($entityFqcn)) {
                     throw new RuntimeException(sprintf('Unable to find the controller related to the "%s" Entity; did you forget to extend "%s"?', $entityFqcn, AbstractCrudController::class));
                 }
